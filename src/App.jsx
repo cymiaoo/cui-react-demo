3e822a01480d719a57f24b649e35225b05163028
cymiaoo <cymiaoo@1996.com>
2019-06-14 21:12:32 +0800

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React,{Component} from 'react'
+import {BrowserRouter,HashRouter,Route,Switch} from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+/* 
+应用根组件
+*/
 
 export default class App extends Component{
     render(){
         return (
-            <div>
-                <h2>hello,react</h2>
-            </div>
+         <BrowserRouter>
+           <Switch>
+           {/* 注册路由 */}
+              <Route path='/login' component={Login}/>
+              <Route path='/' component={Admin}/>
+           </Switch>
+         </BrowserRouter>
+
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+/* 
+入口js    入口js是webpack概念
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './App' //自定义模块引入必须以.开头
 
 ReactDOM.render(<App/>,document.getElementById('root'))
